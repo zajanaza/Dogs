640f8625f7ea851766515b1951ef72bddc6f11d9
zajanaza <janazadrazilova@seznam.cz>
2023-07-26 11:01:06 +0200

    pridana validacni fce pro inputy

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,6 +12,26 @@ export default function Home() {
     race:"",
     age:"",
   });
+  const [valid, setValid] = useState(false);
+  const validateData = (dog) => {
+
+    if (dog.age === "" || parseInt(dog.age) < 0 || parseInt(dog.age) > 24) {
+
+      return setValid(false);
+
+    } else if (dog.name.trim().length === 0) {
+
+      return setValid(false);
+
+    } else if (dog.race.trim().length === 0) {
+
+      return setValid(false);
+
+    }
+
+    setValid(true);
+
+  };
   return (
     <PageContainer>
       <DogList name='dogList'>
