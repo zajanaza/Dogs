0e3e7d5d2fe6ef83ef0a7119ab0b1bde854e9e14
zajanaza <janazadrazilova@seznam.cz>
2023-07-26 10:53:01 +0200

    doplnen formular

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,6 +5,13 @@ import { useState } from "react";
 
 export default function Home() {
   const [listOfDogs, setListOfDogs] = useState(dogs);
+  const [newDog, setNewDog] = useState({
+    id:listOfDogs.length>0 ? Math.max(...listOfDogs.map (dog => dog.id)) + 1 : 1,  
+    //pokud to pole nebude prazdne, tak hleda max id a pak dosadi id plus 1
+    name:"",
+    race:"",
+    age:"",
+  });
   return (
     <PageContainer>
       <DogList name='dogList'>
@@ -18,17 +25,20 @@ export default function Home() {
           <Input 
             type='text' 
             placeholder='jmeno psa' 
-            name='name'/>   
+            name='name'
+            value={newDog.name}/>   
           <Input 
             type='text' 
             placeholder='rasa psa' 
-            name='race'/>  
+            name='race'
+            value={newDog.race}/>  
           <Input 
             type='number' 
             placeholder='vek psa' 
             name='age'
             min='0'
-            max='24'/> 
+            max='24'
+            value={newDog.age}/> 
           <Button>Přidat</Button>
         </DogForm>
         
