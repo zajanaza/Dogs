50e77e1d0f185d5a688ad0357eab3d6f4679fe7f
zajanaza <janazadrazilova@seznam.cz>
2023-07-26 11:08:26 +0200

    propojeni validacni fce s tlacitkem pro pridani noveho psa

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,24 +13,16 @@ export default function Home() {
     age:"",
   });
   const [valid, setValid] = useState(false);
-  const validateData = (dog) => {
-
+  //validacni fce na form inputs, doplnuje hodnotu do valid pomoci setValid a valid je dosazena do Button nize
+  const validateData = (dog) => {                                       
     if (dog.age === "" || parseInt(dog.age) < 0 || parseInt(dog.age) > 24) {
-
       return setValid(false);
-
     } else if (dog.name.trim().length === 0) {
-
       return setValid(false);
-
     } else if (dog.race.trim().length === 0) {
-
       return setValid(false);
-
     }
-
     setValid(true);
-
   };
   return (
     <PageContainer>
@@ -58,8 +50,8 @@ export default function Home() {
             name='age'
             min='0'
             max='24'
-            value={newDog.age}/> 
-          <Button>Přidat</Button>
+            value={newDog.age}/>                      
+          <Button disabled={!valid}>Přidat</Button>
         </DogForm>
         
     </PageContainer>
